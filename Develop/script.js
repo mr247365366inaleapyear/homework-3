9a424ec26fa8105cbf3af182ec6842a7cf152df3
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-01-02 06:20:38 -0500

    added generate code for script.js

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,8 @@ var generateBtn = document.querySelector("#generate");
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberChar = "0123456789";
-var 
-
+var specialChar = "!@#$%^&*()_+-=[]{};':`~,./<>?|";
+var passwordLength;
 
 // Write password to the #password input
 function writePassword() {
@@ -12,7 +12,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
