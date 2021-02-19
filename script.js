e70838425469a31bb2b639f3664236de270cb70f
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-19 16:25:52 -0500

    edited script.js:

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var numberChar = "0123456789";
 var specialChar = "!@#$%^&*()_+-=[]{};':`~,./<>?|";
 var passwordLength;
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
